6fef9f1e05289b5cb45a30f8d4f358bbbe5d0d53
Gilbert McCray <78336419+Mccray1997@users.noreply.github.com>
2023-11-13 17:15:30 -0500

    made event.py and added to homepage

--- homepage.py
+++ homepage.py
@@ -1,10 +1,12 @@
 # homepage.py
 
 import tkinter as tk
+from event import *
 
 
 #Opens the homepage for the event
 def open_homepage(username):
+    global homepage_window
     homepage_window = tk.Toplevel()
     homepage_window.title("Event Organizer Homepage")
 
@@ -23,3 +25,30 @@ def open_homepage(username):
     logout_button = tk.Button(homepage_window,text= "Logout", command=lambda:homepage_window.destroy())
     logout_button.pack(pady = 10)
 
+def create_event_window():
+    global new_event_window
+    registration_window = tk.Toplevel(homepage_window)
+    registration_window.title("User Registration")
+
+    #Entry for new username
+    event_name_label = tk.Label(registration_window, text="New Username:")
+    event_name_label.pack(pady=10)
+    event_name_entry = tk.Entry(registration_window)
+    event_name_entry.pack(pady=10)
+
+
+    #Entry for new password
+    date_label = tk.Label(registration_window, text="New Password:")
+    date_label.pack(pady=10)
+    date_entry = tk.Entry(registration_window, show="*")
+    date_entry.pack(pady=10)
+
+    #create and configure labels and entry fields for email
+    desc_label = tk.Label(registration_window,text= "Email")
+    desc_label.pack(pady=10)
+    desc_entry = tk.Entry(registration_window)
+    desc_entry.pack(pady = 10)
+
+    #Button to complete
+    creation_button = tk.Button(registration_window, text="Register", command=lambda: ())
+    creation_button.pack(pady=10)
