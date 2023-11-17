2b6536cd192367904a0e80d91f57887197f52613
urfavig <150429524+urfavig@users.noreply.github.com>
2023-11-17 12:34:04 -0500

    Add files via upload

---
+++ event.py
@@ -0,0 +1,50 @@
+class Event:
+    def __init__(self,name, date, description):
+        self._name = name
+        self._date = date
+        self._description = description
+        self._speakers = []
+        self._exhibitors = []
+        self._observers = []
+
+    # Getter and Setter for the 'name' attribute
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        self._name = name
+
+    # Getter and Setter for the 'date' attribute
+    def get_date(self):
+        return self._date
+
+    def set_date(self, date):
+        self._date = date
+
+    # Getter and Setter for the 'description' attribute
+    def get_description(self):
+        return self._description
+
+    def set_description(self, description):
+        self._description = description
+
+    def register_speaker(self, speaker_name):
+        self._speakers.append(speaker_name)
+
+    def register_exhibitor(self, exhibitor_name):
+        self._exhibitors.append(exhibitor_name)
+
+    def register_observer(self, observer_name):
+        self._observers.append(observer_name)
+
+    def get_event_details(self):
+        return f"Event: {self._name}\nDate: {self._date}\nDescription: {self._description}"
+
+    def get_speakers(self):
+        return f"Speakers: {', '.join(self._speakers)}"
+
+    def get_exhibitors(self):
+        return f"Exhibitors: {', '.join(self._exhibitors)}"
+
+    def get_observers(self):
+        return f"Observers: {', '.join(self._observers)}"
