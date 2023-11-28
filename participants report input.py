f012125a5f1010e85a14d525e981debfdfd99f59
lftsha04 <151053311+lftsha04@users.noreply.github.com>
2023-11-28 10:12:56 -0500

    Update participants report input.py
    changes text entry to dropdown boxes

--- participants report input.py
+++ participants report input.py
@@ -1,21 +1,21 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import ttk, messagebox
 
 def generate_participant_report():
     category_counts = {}
     total_participants = 0
 
-    for category, entry_var in category_entries.items():
-        count = entry_var.get()
+    for category, combobox in category_comboboxes.items():
+        count = combobox.get()
         if not count.isdigit():
-            messagebox.showwarning("Warning", "Please enter a valid number for each category.")
+            messagebox.showwarning("Warning", "Please select a valid number for each category.")
             return
 
         count = int(count)
         category_counts[category] = count
         total_participants += count
 
-    # Process the information for the participant report (replace this with your actual logic)
+    # Process the information for the participant report 
     participant_result = "Participant Counts Report:\n\n"
     for category, count in category_counts.items():
         participant_result += f"{category}: {count} participants\n"
@@ -23,25 +23,25 @@ def generate_participant_report():
     participant_result += f"\nTotal Participants: {total_participants}"
 
     # Display the participant report
-    participant_report_text.config(state=tk.NORMAL)  # Enable text widget for editing
-    participant_report_text.delete(1.0, tk.END)  # Clear previous content
-    participant_report_text.insert(tk.END, participant_result)  # Insert the new report
-    participant_report_text.config(state=tk.DISABLED)  # Disable text widget for editing
+    participant_report_text.config(state=tk.NORMAL)  
+    participant_report_text.delete(1.0, tk.END)  
+    participant_report_text.insert(tk.END, participant_result)  
+    participant_report_text.config(state=tk.DISABLED)  
 
 root = tk.Tk()
 root.title("Participants Reporter")
 
-# Create and place widgets for participant counts
+# Create and place widgets for participant counts using dropdown boxes with a range from 0 to 50
 participant_categories = ["Exhibitors", "Observers", "Speakers"]
-category_entries = {}
+category_comboboxes = {}
 for i, category in enumerate(participant_categories):
     label = tk.Label(root, text=f"{category}:")
-    entry_var = tk.StringVar()
-    entry = tk.Entry(root, textvariable=entry_var)
-    category_entries[category] = entry_var
+    combobox_var = tk.StringVar()
+    combobox = ttk.Combobox(root, textvariable=combobox_var, values=[str(i) for i in range(51)], state="readonly")
+    category_comboboxes[category] = combobox_var
 
     label.grid(row=i, column=0, padx=10, pady=5, sticky="w")
-    entry.grid(row=i, column=1, padx=10, pady=5)
+    combobox.grid(row=i, column=1, padx=10, pady=5)
 
 participant_generate_button = tk.Button(root, text="Generate Participant Report", command=generate_participant_report)
 participant_generate_button.grid(row=len(participant_categories), column=0, columnspan=2, pady=10)
